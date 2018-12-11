048e16796d79264ff3c73a8333e5e50befcdd24f
thanapattu <45780838+thanapattu@users.noreply.github.com>
2018-12-11 13:36:01 +0530

    Update negative.py

--- negative.py
+++ negative.py
@@ -1,7 +1,7 @@
-num = float (input ("Enter a number:"))
-if num >0:
-print ("positive number ")
-elif num =0;
+num = float (input (""))
+if(num>0):
+ print("positive number ")
+elif(num==0):
   print("zero")
 else:
  print ("negative number ")
