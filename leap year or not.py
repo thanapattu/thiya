78b5b02cb2e799810720f894d920b6203be184e7
thanapattu <45780838+thanapattu@users.noreply.github.com>
2019-01-25 13:46:01 +0530

    Create leap year or not.py

---
+++ leap year or not.py
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int yr;
+  printf ("Enter a year \n");
+  scanf ("%d", &yr);
+
+  if (yr%4 == 0) {
+
+      if(yr%100 == 0) {
+      
+          if(yr%400 == 0)
+             printf("\n It is LEAP YEAR.");
+          else
+             printf("\n It is NOT LEAP YEAR.");
+      }
+
+      else {
+             printf ("\n It is LEAP YEAR.");
+      }
+  }
+  else
+      printf("\n It is NOT LEAP YEAR.");
+  return();
+  
