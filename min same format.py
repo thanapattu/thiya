660efb09577edf134cb9f55d14d649a30c5dad29
thanapattu <45780838+thanapattu@users.noreply.github.com>
2019-02-11 14:46:22 +0530

    Create min same format.py

---
+++ min same format.py
@@ -0,0 +1,16 @@
+hours = (diff.seconds) / 3600  
+print (str(hours) + ' Hours')
+minutes = (diff.seconds) / 60
+print (str(minutes) + ' Minutes')
+print (str(diff.seconds) + ' secs')hours = (diff.seconds) / 3600  
+print (str(hours) + ' Hours')
+ 
+ 
+#same for minutes just divide the seconds by 60
+ 
+minutes = (diff.seconds) / 60
+print (str(minutes) + ' Minutes')
+ 
+#to print seconds, you know already ;)
+ 
+print (str(diff.seconds) + ' secs')
