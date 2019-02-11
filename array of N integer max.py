1272f62745f55fb0ede355e041add00205ee6a29
thanapattu <45780838+thanapattu@users.noreply.github.com>
2019-02-11 13:28:20 +0530

    Rename array of N integer.py to array of N integer max.py

---
+++ array of N integer max.py
@@ -0,0 +1,7 @@
+number_array = list()
+number = input("Enter the number of elements you want:")
+print ('Enter numbers in array:')
+for i in range(int(number));
+    n = input("number:")
+    number_array.append(int(n))
+print ('ARRAY:',number_array)
