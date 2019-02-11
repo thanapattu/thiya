edbe4d95e0b259c80e1b85319b970f56df2f08db
thanapattu <45780838+thanapattu@users.noreply.github.com>
2019-02-11 13:28:59 +0530

    Delete array of N integer.py

--- array of N integer.py
+++
@@ -1,7 +0,0 @@
-number_array = list()
-number = input("Enter the number of elements you want:")
-print ('Enter numbers in array: ')
-for i in range(int(number)):
-    n = input("number :")
-    number_array.append(int(n))
-print ('ARRAY: ',number_array)
