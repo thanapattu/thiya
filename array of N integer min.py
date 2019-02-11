879b5ccb1bab6e94549cce8a1331df0147bc023e
thanapattu <45780838+thanapattu@users.noreply.github.com>
2019-02-11 13:32:29 +0530

    Create array of N integer min.py

---
+++ array of N integer min.py
@@ -0,0 +1,9 @@
+int main() 
+{ 
+    int arr[] = {198, 76, 544, 123, 154, 675}; 
+    int n = 2; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    count << "Minimum product is "
+         << minProduct(arr,n,n); 
+    return 0; 
+} 
