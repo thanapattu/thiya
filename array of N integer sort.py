b47690dda91d81bf7824ed2ab17d686c07108fb4
thanapattu <45780838+thanapattu@users.noreply.github.com>
2019-02-11 13:38:56 +0530

    Create array of N integer sort.py

---
+++ array of N integer sort.py
@@ -0,0 +1,12 @@
+int main() 
+{ 
+    int arr[] = { 10, 7, 9, 2, 8, 3, 5, 4, 6, 1 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+  
+    // for sort an array 
+    sortit(arr,n); 
+  
+    // for print all the element in sorted way 
+    for (int i = 0; i < n; i++)  
+        count << arr[i] << " ";     
+} 
