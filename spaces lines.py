8e5382477ecfc0b5def38b5deec3adb137c8405f
thanapattu <45780838+thanapattu@users.noreply.github.com>
2019-02-25 14:15:14 +0530

    Create spaces lines.py

---
+++ spaces lines.py
@@ -0,0 +1,11 @@
+fname = input (Enter file name:")
+k=0
+with open (fname,'r') as f:
+   for line in f :
+     word = line split()
+     for i in words 
+       for letter in i:
+        if (letter isspace):
+           k=k+1
+print (" occurences of blank spaces:")
+print (k)
